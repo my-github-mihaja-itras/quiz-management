5331b6f88a3d0637c3f45e514f335b425c326fca
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2024-04-21 17:55:10 +0300

    [quizResult] fix orthographe #main

--- src/components/shared/quizResult/quizResult.component.tsx
+++ src/components/shared/quizResult/quizResult.component.tsx
@@ -12,7 +12,7 @@ const QuizResult = ({ quizSession }: { quizSession: QuizSession | any }) => {
         (quizItem: QuestionResult, quizItemIndex: number) => (
           <div className={style.quizItem} key={`quizItem${quizItemIndex}`}>
             <div className={style.question}>
-              {`Q ${quizItemIndex + 1} ) `} {quizItem.question.questionAsked}
+              {`Question ${quizItemIndex + 1} ) `} {quizItem.question.questionAsked}
             </div>
             {quizItem.question.choice.map((choice, choiceIndex) => (
               <div
