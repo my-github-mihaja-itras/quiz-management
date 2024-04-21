8553958d37b2423f03616bab4fafd79dfb5b008b
MihajaUser <81333217+MihajaUser@users.noreply.github.com>
2024-04-21 07:55:41 +0300

    fix type

--- src/services/question/question.models.ts
+++ src/services/question/question.models.ts
@@ -2,14 +2,14 @@ export interface QuestionType {
   _id?: string;
   questionNumber?: number;
   questionAsked: string;
-  choice: string[];
+  choice: string[] | ChoiceOptions[];
   trueAnswer: number;
   wasUsedDate?: Date;
 }
 
-// export interface ChoiceOptions {
-//   choiceOptions: string;
-// }
+export interface ChoiceOptions {
+  choiceOptions: string;
+}
 
 export interface QuestionTypeToInsert {
   questionAsked: string;
