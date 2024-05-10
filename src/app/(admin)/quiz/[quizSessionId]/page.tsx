429ecc6499d4984cd5808f9c195197db954e99f6
MihajaItras <mihaja.randrianarisoa@dreamslab.fr>
2024-05-10 09:36:44 +0300

    f

--- src/app/(admin)/quiz/[quizSessionId]/page.tsx
+++ src/app/(admin)/quiz/[quizSessionId]/page.tsx
@@ -3,25 +3,15 @@
 import Loader from "@/components/loader/loader";
 import ErrorModal, { ErrorMessage } from "@/components/modal/errorModal";
 import DetailsSection from "@/components/shared/details-section/details.section.components";
-import Details from "@/components/shared/details/details.components";
-import FormFieldsEditable from "@/components/shared/form-fields/form.fields.components";
-import Tabs from "@/components/shared/tabs/tabs.components";
-import { ActionType } from "@/cores/constant/constant.history";
 import UseWindowSize from "@/cores/window/window.size";
-
-import { GetStudentById } from "@/services/student/student.service";
-import { EditUserById, getUserById } from "@/services/user/user-service";
-import extractTokenInfo from "@/utils/extract.token";
 import { getLocalStorageItem } from "@/utils/localStorage.utils";
 import { useEffect, useState } from "react";
 import { ServerResponse } from "@/cores/constant/response.constant";
 import { HttpStatusCode } from "axios";
 import { getQuizSessionById } from "@/services/quiz-session/quiz-session.service";
 import QuizResult from "@/components/shared/quizResult/quizResult.component";
-import IconGear from "@/components/shared/icons/iconGear";
 import Processing from "@/components/shared/processing/processing.component";
 import { QuizSession } from "@/services/quiz-session/quiz-session.models";
-import UserQuizFormFields from "@/components/form/userQuiz.form.fields";
 import { User } from "@/services/user/user.models";
 import IconQuiz from "@/components/shared/icons/IconQuiz";
 
@@ -30,15 +20,10 @@ const StudentDetail = ({ params }: { params: { quizSessionId: string } }) => {
   const [userData, setUserData] = useState<User | any>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [formFieldsData, setFormFieldsData] = useState<any>();
-  const [fieldsIsDisabled, setFieldsIsEditable] = useState<boolean>(true);
-
   const [dataNotFound, setDataNotFound] = useState<boolean>(false);
   const token = getLocalStorageItem("loginAccessToken") || "";
-  const tokenInfo: any = extractTokenInfo(token);
   const [isOpen, setIsOpen] = useState<Boolean>(false);
-  const [isSuccess, setIsSuccess] = useState<boolean>(false);
   const [message, setMessage] = useState<ErrorMessage>();
-
   const closeModal = (value: boolean) => {
     setIsOpen(value);
   };
@@ -66,34 +51,9 @@ const StudentDetail = ({ params }: { params: { quizSessionId: string } }) => {
     setIsLoading(false);
   };
 
-  const getUserData = async (userId: string) => {
-    const response: ServerResponse = await getUserById(userId);
-    // console.log(response);
-    if (response.status === HttpStatusCode.Ok) {
-      setDataNotFound(false);
-      setIsLoading(false);
-      const user: User = response?.data.data;
-
-      setUserData(user);
-      setFormFieldsData({
-        username: user?.username,
-        photo: user?.photo,
-      });
-    } else {
-      setDataNotFound(true);
-      setIsLoading(false);
-    }
-    setIsLoading(false);
-  };
-
   useEffect(() => {
     getQuizSessionDataById();
   }, []);
-
-  const handleChangeEditableFields = () => {
-    setFieldsIsEditable(!fieldsIsDisabled);
-  };
- 
 
   const screenSize = UseWindowSize();
 
